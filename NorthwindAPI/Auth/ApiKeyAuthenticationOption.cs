a1ddd1c538cd5e5bde45da1e651b55d5371ca3ff
Tóth László <lastoth@gmail.com>
2024-11-11 10:30:28 +0100

    Auth option

---
+++ NorthwindAPI/Auth/ApiKeyAuthenticationOption.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace NorthwindAPI.Auth
+{
+    public class ApiKeyAuthenticationOption : AuthenticationSchemeOptions
+    {
+        public const string DefaultScheme = "API Key";
+        public string Scheme => DefaultScheme;
+        public string AuthenticationType = DefaultScheme;
+
+    }
+}
